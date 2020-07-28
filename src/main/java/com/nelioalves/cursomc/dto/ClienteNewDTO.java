a817d3faa9adbdbf2c5252520706f264c9172391
Nitai Charan <nitaicharan@gmail.com>
2020-07-28 18:58:06 -0300

    45. Validação customizada: CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -5,32 +5,63 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 import com.nelioalves.cursomc.domain.Cidade;
 import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Endereco;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
+import com.nelioalves.cursomc.services.validation.ClienteInsert;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@ClienteInsert
 @NoArgsConstructor
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
+
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
+
 	private String complemento;
+
 	private String bairro;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+
 	private String telefone2;
+	
 	private String telefone3;
+
 	private Integer cidadeId;
 
 	public Cliente toCliente() {
