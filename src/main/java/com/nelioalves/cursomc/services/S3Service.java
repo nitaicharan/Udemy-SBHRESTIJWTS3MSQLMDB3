d0ca7765ab9482591d1c9d14793323db5d8795ca
Nitai Charan <nitaicharan@gmail.com>
2020-08-17 20:29:36 -0300

    84. Salvando primeiro arquivo no S3

---
+++ src/main/java/com/nelioalves/cursomc/services/S3Service.java
@@ -0,0 +1,52 @@
+package com.nelioalves.cursomc.services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.nelioalves.cursomc.services.exceptions.FileException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class S3Service {
+
+    @Autowired
+    private AmazonS3 s3client;
+
+    @Value("${s3.bucket}")
+    private String bucketName;
+
+    public URI uploadFile(MultipartFile multipartFile) {
+        try {
+            String fileName = multipartFile.getOriginalFilename();
+            InputStream is = multipartFile.getInputStream();
+            String contentType = multipartFile.getContentType();
+            return uploadFile(is, fileName, contentType);
+        } catch (IOException e) {
+            throw new FileException("Erro de IO: " + e.getMessage());
+        }
+    }
+
+    public URI uploadFile(InputStream is, String fileName, String contentType) {
+        try {
+            ObjectMetadata meta = new ObjectMetadata();
+            meta.setContentType(contentType);
+            log.info("Iniciando upload");
+            s3client.putObject(bucketName, fileName, is, meta);
+            log.info("Upload finalizado");
+            return s3client.getUrl(bucketName, fileName).toURI();
+        } catch (URISyntaxException e) {
+            throw new FileException("Erro ao converter URL para URI");
+        }
+    }
+}
