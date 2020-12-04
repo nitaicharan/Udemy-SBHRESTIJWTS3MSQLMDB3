03fc93a649b11e3d077fcbacf278fb7ccba37fdd
Nitai Charan <nitaicharan@gmail.com>
2020-12-04 17:28:52 -0300

    200. Documentação com Swagger - Parte 1

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,6 +37,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/v3/api-docs" };
     private static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**", "/auth/forgot/**" };
     private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**", "/estados/**" };
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(//
+                "/v2/api-docs"//
+                , "/configuration/ui"//
+                , "/swagger-resources/**"//
+                , "/configuration/**"//
+                , "/swagger-ui.html"//
+                , "/webjars/**"//
+        );
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
